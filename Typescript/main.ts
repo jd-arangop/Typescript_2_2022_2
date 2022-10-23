8830e85f4094e00b802f470e10386dfbeb7e5f13
Juan Arango <jd.arangop@uniandes.edu.co>
2022-10-23 11:51:05 -0500

    Taller 2

---
+++ Typescript/main.ts
@@ -0,0 +1,53 @@
+import { Serie } from "./Serie.js";
+
+import { dataSeries } from "./data.js";
+
+const seriesTbody: HTMLElement = document.getElementById("series")!;
+const cardDescription: HTMLElement = document.getElementById("card")!;
+
+renderSeriesInTable(dataSeries);
+renderAverageSeasons(getAverageSeasons(dataSeries));
+
+
+function renderSeriesInTable(dataSeries: Serie[]): void {
+    dataSeries.forEach(serie => {
+        let trElement = document.createElement("tr");
+        trElement.innerHTML = `<td>${serie.id}</td>
+                                <td>
+                                    <button type="button" class="btn btn-link" id="button-serie-${serie.id}">${serie.nombre}</button>
+                                </td>
+                                <td>${serie.canal}</td>
+                                <td>${serie.temporadas}</td>`;
+        seriesTbody.appendChild(trElement);
+
+        let btnSerie: HTMLElement = document.getElementById("button-serie-" + serie.id)!;
+        btnSerie.onclick = () => renderCardDescription(dataSeries, serie.id - 1);
+    });
+}
+
+function getAverageSeasons(dataSeries: Serie[]): number {
+    let averageSeasons: number = 0;
+    dataSeries.forEach(serie => {
+        averageSeasons = averageSeasons + serie.temporadas;
+    });
+    averageSeasons = averageSeasons / dataSeries.length;
+    return averageSeasons;
+}
+
+function renderAverageSeasons(seasons: number): void {
+    let trElement = document.createElement("tr");
+    trElement.innerHTML = `<td>Temporadas promedio: ${seasons}</td>`
+    seriesTbody.appendChild(trElement);
+}
+
+function renderCardDescription(dataSeries: Serie[], id: number): void {
+    let serie: Serie = dataSeries[id];
+    cardDescription.innerHTML = `<img class="card-img-top" src=${serie.imagen} alt=${serie.imagen}>
+                                <div class="card-body">
+                                    <h5 class="card-title">${serie.nombre}</h5>
+                                    <p class="card-text">${serie.sinopsis}</p>
+                                </div>
+                                <div class="card-body">
+                                    <a href=${serie.web} class="card-link">Ver en ${serie.canal}</a>
+                                </div>`;
+}
---
+++ scripts/Typescript/main.js
@@ -0,0 +1,31 @@
+import { dataSeries } from "./data.js";
+var seriesTbody = document.getElementById("series");
+var cardDescription = document.getElementById("card");
+renderSeriesInTable(dataSeries);
+renderAverageSeasons(getAverageSeasons(dataSeries));
+function renderSeriesInTable(dataSeries) {
+    dataSeries.forEach(function (serie) {
+        var trElement = document.createElement("tr");
+        trElement.innerHTML = "<td>".concat(serie.id, "</td>\n                                <td>\n                                    <button type=\"button\" class=\"btn btn-link\" id=\"button-serie-").concat(serie.id, "\">").concat(serie.nombre, "</button>\n                                </td>\n                                <td>").concat(serie.canal, "</td>\n                                <td>").concat(serie.temporadas, "</td>");
+        seriesTbody.appendChild(trElement);
+        var btnSerie = document.getElementById("button-serie-" + serie.id);
+        btnSerie.onclick = function () { return renderCardDescription(dataSeries, serie.id - 1); };
+    });
+}
+function getAverageSeasons(dataSeries) {
+    var averageSeasons = 0;
+    dataSeries.forEach(function (serie) {
+        averageSeasons = averageSeasons + serie.temporadas;
+    });
+    averageSeasons = averageSeasons / dataSeries.length;
+    return averageSeasons;
+}
+function renderAverageSeasons(seasons) {
+    var trElement = document.createElement("tr");
+    trElement.innerHTML = "<td>Temporadas promedio: ".concat(seasons, "</td>");
+    seriesTbody.appendChild(trElement);
+}
+function renderCardDescription(dataSeries, id) {
+    var serie = dataSeries[id];
+    cardDescription.innerHTML = "<img class=\"card-img-top\" src=".concat(serie.imagen, " alt=").concat(serie.imagen, ">\n                                <div class=\"card-body\">\n                                    <h5 class=\"card-title\">").concat(serie.nombre, "</h5>\n                                    <p class=\"card-text\">").concat(serie.sinopsis, "</p>\n                                </div>\n                                <div class=\"card-body\">\n                                    <a href=").concat(serie.web, " class=\"card-link\">Ver en ").concat(serie.canal, "</a>\n                                </div>");
+}
